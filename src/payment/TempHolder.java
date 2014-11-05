b9b3ca314ab6fb0de9553fe8dbf12e85024dd595
Emmanuel Nyachoke <achachiez@gmail.com>
2014-11-05 15:51:50 +0300

    Removed conflicting src files

--- src/payment/TempHolder.java
+++
@@ -1,2 +0,0 @@
-//It may not be a good idea to use getters and setters, because the values are different and vales may be changes by another user between the calls
-//so use a database to for futures transfer. using a trigger
